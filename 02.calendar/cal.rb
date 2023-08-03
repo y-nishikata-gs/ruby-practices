2f6db8a32836f976d3b7b6ec9b2056fd7589a36c
y-nishikata-gs <y.nishikata@genbasupport.com>
2023-08-03 17:41:58 +0900

    文字列出力、条件分岐のリファクタリング

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -3,7 +3,10 @@ require 'date'
 
 today = Date.today
 
-# 日付を条件に応じて1日ずつ出力
+def get_day_format(day)
+    day < 10 ? " #{day} " : "#{day} " # 1桁の日付は左にスペース
+end
+
 def create_cal(month:, year:)
     start_date = Date.new(year, month, 1)
     end_date = Date.new(year, month, -1)
@@ -11,43 +14,29 @@ def create_cal(month:, year:)
     puts "      #{month}月 #{year}"
     puts "日 月 火 水 木 金 土"
 
+    print " " * (start_date.wday * 3)
     (start_date..end_date).each do |date|
         if date.saturday?
-            if date.day < 10 # 1桁の日付は左にスペース
-                if date == start_date
-                    puts date.day.to_s.rjust(date.wday * 3 + 2) # 初日は曜日に合わせてレイアウトを調整
-                else
-                    puts " " + date.day.to_s + " "
-                end
-            else
-                puts date.day.to_s + " "
-            end
+            puts get_day_format(date.day)
         else
-            if date.day < 10 # 1桁の日付は左にスペース
-                if date == start_date
-                    print date.day.to_s.rjust(date.wday * 3 + 2) + " " # 初日は曜日に合わせてレイアウトを調整
-                else
-                    print " " + date.day.to_s + " "
-                end
-            else
-                print date == end_date ? date.day.to_s + " \n" : date.day.to_s + " "
-            end
+            print get_day_format(date.day)
         end
     end
+    print " \n"
 end
 
 options_bool = {}
 options_value = {}
 
-# オプションが指定されているか
+# オプションが指定されているか判定するために取得
 opt_bool = OptionParser.new
 opt_bool.on('-m', '--month') {|v| options_bool[:m] = v }
 opt_bool.on('-y', '--year') {|v| options_bool[:y] = v }
 opt_bool.parse(ARGV)
 
 # オプション指定の真偽値
-m_has_value = (options_bool[:m] == true)
-y_has_value = (options_bool[:y] == true)
+m_has_value = (options_bool[:m])
+y_has_value = (options_bool[:y])
 
 # オプション引数
 opt_val = OptionParser.new
@@ -55,15 +44,7 @@ opt_val.on('-m VALUE', '--month') {|v| options_value[:m] = v.to_i }
 opt_val.on('-y VALUE', '--year') {|v| options_value[:y] = v.to_i }
 opt_val.parse(ARGV)
 
+cal_month = m_has_value ? options_value[:m] : today.month
+cal_year = y_has_value ? options_value[:y] : today.year
 
-# オプションに応じてカレンダーを表示
-if m_has_value && y_has_value
-# 年月を指定
-    create_cal(month: options_value[:m], year: options_value[:y])
-elsif m_has_value
-# 月を指定
-    create_cal(month: options_value[:m], year: today.year)
-else
-# 指定なし
-    create_cal(month: today.month, year: today.year)
-end
+create_cal(month: cal_month, year: cal_year)
