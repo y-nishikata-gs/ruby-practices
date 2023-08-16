5f8705f1ee9823c2c7eb4902f7928250630ead7a
y-nishikata-gs <y.nishikata@genbasupport.com>
2023-08-16 11:40:59 +0900

    不要なif文の廃止、メソッド名・コメントの修正

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -4,30 +4,29 @@ score = ARGV[0]
 scores = score.split(',')
 point = 0
 
-def get_score(score)
+def convert_score(score)
   score == 'X' ? 10 : score.to_i
 end
 
 def add_bonus(score1 = nil, score2 = nil)
   bonus_point = 0
-  bonus_point += get_score(score1) if !score1.nil?
-  bonus_point += get_score(score2) if !score1.nil?
+  bonus_point += convert_score(score1)
+  bonus_point += convert_score(score2)
   bonus_point
 end
 
 scores_index = 0
 10.times do |n|
-  point += get_score(scores[scores_index])
+  point += convert_score(scores[scores_index])
   if n == 10 # スペア・ストライクの場合は3投目があるため
-    point += get_score(scores[scores_index + 1])
-    point += get_score(scores[scores_index + 2])
-
-  elsif scores[scores_index] == 'X' # ストライク
+    point += convert_score(scores[scores_index + 1])
+    point += convert_score(scores[scores_index + 2])
+  elsif scores[scores_index] == 'X'
     point += add_bonus(scores[scores_index + 1], scores[scores_index + 2])
     scores_index += 1
   else
-    point += get_score(scores[scores_index + 1])
-    point += add_bonus(scores[scores_index + 2]) if get_score(scores[scores_index]) + get_score(scores[scores_index + 1]) == 10 # スペア
+    point += convert_score(scores[scores_index + 1])
+    point += add_bonus(scores[scores_index + 2]) if convert_score(scores[scores_index]) + convert_score(scores[scores_index + 1]) == 10
     scores_index += 2
   end
 end
